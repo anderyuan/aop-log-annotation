5a75e910f1711046a1f49d882dbc3d95a288bf04
wangfj <wangfj@chinawiserv.com>
2018-12-07 16:07:05 +0800

    初始化spring-boot项目

---
+++ src/main/java/com/wangfj/aoplogannotation/AopLogAnnotationApplication.java
@@ -0,0 +1,12 @@
+package com.wangfj.aoplogannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopLogAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopLogAnnotationApplication.class, args);
+	}
+}
